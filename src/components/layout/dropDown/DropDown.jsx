b923c749a00f36c5814acdc5760cd32756fda977
nikita <nikita.bobers@gmail.com>
2020-05-06 16:50:13 +0200

    drop down menu

--- src/components/layout/dropDown/DropDown.jsx
+++ src/components/layout/dropDown/DropDown.jsx
@@ -1,15 +1,63 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import FontAwesome from "react-fontawesome";
 import "./dropDown.css";
 
-function DropDown() {
+const DropDown = ({ title, list }) => {
+  let initialState = {
+    listOpen: false,
+    headerTitle: title,
+  };
+
+  const [state, setState] = useState(initialState);
+
+  const toggleList = () => {
+    setState((prevState) => ({
+      listOpen: !prevState.listOpen,
+      headerTitle: title,
+    }));
+  };
+
+  const handleClickOutside = (event) => {
+    if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+      setState({
+        listOpen: false,
+        headerTitle: title,
+      });
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  const { headerTitle, listOpen } = state;
+
+  const wrapperRef = useRef(null);
+
   return (
-    <div className="drop-menu">
-      <ul className="drop-menu__list">
-        <li className="drop-menu__item">item</li>
-        <li className="drop-menu__item">item</li>
-      </ul>
+    <div className="dd-wrapper" ref={wrapperRef}>
+      <div className="dd-header" onClick={() => toggleList()}>
+        <div className="dd-header-title">{headerTitle}</div>
+        {listOpen ? (
+          <FontAwesome name="angle-up" size="lg" />
+        ) : (
+          <FontAwesome name="angle-down" size="lg" />
+        )}
+      </div>
+      {listOpen && (
+        <ul className="dd-list">
+          {list.map((item) => (
+            <li className="dd-list-item" key={item.id}>
+              {item.title}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-}
+};
 
 export default DropDown;
--- src/components/pages/shop/menu/ShopMenu.jsx
+++ src/components/pages/shop/menu/ShopMenu.jsx
@@ -3,9 +3,32 @@ import DropDown from "../../../layout/dropDown/DropDown.jsx";
 import "./shopMenu.css";
 
 const ShopMenu = () => {
+  const data = {
+    categories: [
+      {
+        id: 0,
+        title: "Bag",
+        selected: false,
+        key: "categories",
+      },
+      {
+        id: 1,
+        title: "Clothing",
+        selected: false,
+        key: "categories",
+      },
+      {
+        id: 3,
+        title: "Accessories",
+        selected: false,
+        key: "categories",
+      },
+    ],
+  };
+
   return (
     <div className="shop-menu">
-      <DropDown />
+      <DropDown title="Categories" list={data.categories} />
     </div>
   );
 };
