f9a2ee4764be1997ae0ed95ae95aec150e6b5969
nikita <nikita.bobers@gmail.com>
2020-07-01 22:31:05 +0300

    shop list filter dropDown data from json

--- client/src/components/pages/shop/menu/ShopMenu.jsx
+++ client/src/components/pages/shop/menu/ShopMenu.jsx
@@ -1,56 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DropDown from "../../../layout/dropDown/DropDown.jsx";
+import { useSelector } from "react-redux";
+// import { listProducts } from "../../../../actions/productActions";
 import "./shopMenu.css";
 
 const ShopMenu = () => {
-  const data = {
-    categories: [
-      {
-        id: 0,
-        title: "Bag",
-        selected: false,
-        key: "categories",
-      },
-      {
-        id: 1,
-        title: "Clothing",
-        selected: false,
-        key: "categories",
-      },
-      {
-        id: 3,
-        title: "Accessories",
-        selected: false,
-        key: "categories",
-      },
-    ],
-    colors: [
-      {
-        id: 0,
-        title: "Black",
-        selected: false,
-        key: "colors",
-      },
-      {
-        id: 1,
-        title: "White",
-        selected: false,
-        key: "colors",
-      },
-      {
-        id: 2,
-        title: "Purple",
-        selected: false,
-        key: "colors",
-      },
-    ],
+  const [categories, setCategories] = useState([]);
+  const [color, setColor] = useState([]);
+
+  const productList = useSelector((state) => state.productList);
+
+  const { products } = productList;
+
+  const filterUndefined = (array) =>
+    array.filter((element) => element !== undefined);
+
+  const getParameters = (array) => {
+    const categoriesData = filterUndefined([
+      ...new Set(array.map((x) => x.categories)),
+    ]);
+
+    const colorData = filterUndefined([...new Set(array.map((x) => x.color))]);
+
+    setCategories(categoriesData);
+
+    setColor(colorData);
   };
+
+  useEffect(() => {
+    getParameters(products);
+  }, [products]);
 
   return (
     <div className="shop-menu">
       <div className="shop-menu__items">
-        <DropDown title="Categories" list={data.categories} />
-        <DropDown title="Colors" list={data.colors} />
+        <DropDown
+          title={<p>Categories</p>}
+          list={categories}
+          border={true}
+          hover={true}
+        />
+        <DropDown
+          title={<p>Colors</p>}
+          list={color}
+          border={true}
+          hover={true}
+        />
       </div>
     </div>
   );
