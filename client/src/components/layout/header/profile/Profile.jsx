71958b569d1f276526dbbffd1ff380e3578ab5cc
nikita <nikita.bobers@gmail.com>
2020-08-11 12:03:10 +0300

    cart icon with item counter

--- client/src/components/layout/header/profile/Profile.jsx
+++ client/src/components/layout/header/profile/Profile.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./profile.css";
 import { Link } from "react-router-dom";
 
 const Profile = () => {
+	const cart = useSelector((state) => state.cart);
+
+	const { cartItems } = cart;
+
 	return (
 		<div className="profile__menu">
-			{/* <div className="profile__item">
-                 <i className="far fa-user-circle fa-lg"></i>
-             </div> */}
 			<div className="profile__item">
-				<Link to="/cart">
+				<Link to="/cart" className="cart-icon">
 					<i className="fas fa-shopping-bag fa-lg"></i>
 				</Link>
+				<span className="cart-count">{cartItems.length}</span>
 			</div>
 		</div>
 	);
