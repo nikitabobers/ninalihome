a9f759c11d9f39f963a26fb1c280c3b3e3b76573
nikita <nikita.bobers@gmail.com>
2020-08-13 21:24:44 +0300

    cart with items layout

--- client/src/components/pages/cart/Cart.jsx
+++ client/src/components/pages/cart/Cart.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { CartItem } from "./cartItem/cartItem";
+import { CartItem } from "./cartItem/CartItem";
 import { removeItemFromCart } from "../../../actions/cartActions";
 import { Loader } from "../../layout/loader/Loader";
 import "./cart.css";
@@ -12,10 +12,15 @@ const Cart = (props) => {
 
 	const dispatch = useDispatch();
 
-	const removeItem = () => {
-		dispatch(removeItemFromCart());
+	const removeItem = (id) => {
+		dispatch(removeItemFromCart(id));
 	};
-	console.log(cartItems);
+
+	const calcTotal = () => {
+		const prices = cartItems.map((item) => item.price);
+		const sum = prices.reduce((acc, val) => acc + val);
+		return sum;
+	};
 
 	return (
 		<div className="container">
@@ -28,18 +33,25 @@ const Cart = (props) => {
 						{cartItems.map((item) => (
 							<CartItem
 								key={item.id}
+								id={item.id}
 								name={item.name}
 								categories={item.categories}
 								price={item.price}
 								image={item.images[0]}
 								color={item.color}
 								size={item.size}
-								qty="1"
+								qty={item.qty}
+								onClick={() => removeItem(item.id)}
 							/>
 						))}
 					</div>
 					<div className="cart__section-pay">
 						<h2 className="">Summary</h2>
+						<div className="cart__section-promo">Do you have Promo Code?</div>
+						<div className="cart__section-total">
+							<p>Total:</p>
+							<span>{cartItems.length > 0 ? calcTotal() : 0}$</span>
+						</div>
 					</div>
 				</div>
 			)}
