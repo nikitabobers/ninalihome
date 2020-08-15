f24da0d00aa80751ebe1ea230ca9ecc64f48bf67
nikita <nikita.bobers@gmail.com>
2020-08-15 13:14:47 +0300

    cart item quantity from LC

--- client/src/reducers/cartReducer.js
+++ client/src/reducers/cartReducer.js
@@ -1,42 +1,41 @@
 import {
 	CART_ADD_ITEM,
-	CART_REQUEST,
 	CART_DELETE_ITEM,
+	CART_COUNT_TOTAL_PRICE,
 } from "../constants/cartConstants";
 
 const initialState = {
 	cartItems: [],
-	quantityItems: {},
-	loading: true,
-};
-
-const addItems = (state = initialState.cartItems, action) => {
-	switch (action.type) {
-		case CART_ADD_ITEM:
-			return [...state, action.payload];
-		default:
-			return state;
-	}
-};
-
-const addItemQuantity = (state = initialState.quantityItems, action) => {
-	switch (action.type) {
-		case CART_ADD_ITEM:
-			return { ...state };
-		default:
-			return state;
-	}
+	total: null,
 };
 
 const cartReducer = (state = initialState, action) => {
 	switch (action.type) {
-		case CART_REQUEST:
-			return initialState;
-		default:
+		case CART_ADD_ITEM:
+			return { cartItems: [...state.cartItems, action.payload] };
+		case CART_DELETE_ITEM:
 			return {
-				cartItems: addItems(state.cartItems, action),
-				quantityItems: addItemQuantity(state.quantityItems, action),
+				cartItems: state.cartItems.filter((item) => item.id !== action.payload),
 			};
+		case CART_COUNT_TOTAL_PRICE:
+			const prices = state.cartItems.map((item) => item.price);
+
+			const qty = state.cartItems.map((item) => item.qty);
+
+			let itemResult = [];
+
+			for (let i = 0; i < prices.length; i++) {
+				itemResult.push(prices[i] * qty[i]);
+			}
+
+			const sum = itemResult.reduce((acc, val) => acc + val);
+
+			return {
+				...state,
+				total: sum,
+			};
+		default:
+			return state;
 	}
 };
 
