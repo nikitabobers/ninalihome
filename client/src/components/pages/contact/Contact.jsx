e05e59c047a5502e09a3440ff4c6fee92bbf0c86
nikita <nikita.bobers@gmail.com>
2020-08-16 16:26:55 +0300

    contact page layout; 404 page button added

--- client/src/components/pages/contact/Contact.jsx
+++ client/src/components/pages/contact/Contact.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import "./contact.css";
 
 const Contact = () => {
-	return <div>Contact page</div>;
+	return (
+		<div className="container">
+			<div className="contact--section">
+				<h1>Contact Us</h1>
+				<form className="contact--cart">
+					<label className="contact--label">
+						Name
+						<input type="text" placeholder="First name" />
+					</label>
+					<label className="contact--label">
+						Email Address
+						<input type="email" placeholder="name@email.com" />
+					</label>
+					<label className="contact--label">
+						Message subject
+						<input type="text" placeholder="Subject" />
+					</label>
+					<label className="contact--label">
+						Message
+						<textarea placeholder="Your message" />
+					</label>
+					<input className="btn btn--black" type="submit" value="Submit" />
+				</form>
+			</div>
+		</div>
+	);
 };
 
 export default Contact;
