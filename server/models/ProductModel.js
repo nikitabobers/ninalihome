bac2c5b251f30b019f86112d57904a2c651f86b2
nikita <nikita.bobers@gmail.com>
2020-09-16 22:27:16 +0300

    Routes/Models admin and product

---
+++ server/models/ProductModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const prodctSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    price: { type: Number, default: 0, required: true },
+    category: { type: String, required: true },
+    countInStock: { type: Number, default: 0, required: true },
+    description: { type: String, required: true },
+});
+
+const Product = mongoose.model("Product", prodctSchema);
+
+module.exports = Product;
---
+++ server/routes/userRoute.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+const User = require("../models/UserModel");
+
+router.post(
+    "/register",
+    [
+        check("name", "Please add name").not().isEmpty(),
+        check(
+            "password",
+            "Please enter a password with 3 or more characters"
+        ).isLength({ min: 3 }),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const { name, password } = req.body;
+
+            let user = await User.findOne({ name });
+
+            user = new User({
+                name: "admin",
+                password: "1234",
+                isAdmin: true,
+            });
+
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            const newUser = await user.save();
+
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            };
+
+            jwt.sign(
+                payload,
+                config.get("jwtSecret"),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+
+            res.send(newUser);
+        } catch (error) {
+            res.send({ message: error.message });
+        }
+    }
+);
+
+module.exports = router;
