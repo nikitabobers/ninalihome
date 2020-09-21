86bfff13f479ca32523e4d856b10f64c02627115
nikita <nikita.bobers@gmail.com>
2020-09-21 22:50:49 +0300

    header and footer hide on login page

--- client/src/components/layout/header/Header.jsx
+++ client/src/components/layout/header/Header.jsx
@@ -4,21 +4,29 @@ import Profile from "./profile/Profile";
 import { Link } from "react-router-dom";
 import "./header.css";
 
+const checkRoute = () => {
+    if (window.location.pathname === "/admin/login") {
+        return null;
+    } else {
+        return (
+            <div className="header">
+                <div className="container">
+                    <div className="header__bottom-window">
+                        <Link to="/" className="header__logo">
+                            ninalihome
+                        </Link>
+                        <Navbar />
+                        <Profile />
+                    </div>
+                    <hr />
+                </div>
+            </div>
+        );
+    }
+};
+
 const Header = () => {
-	return (
-		<div className="header">
-			<div className="container">
-				<div className="header__bottom-window">
-					<Link to="/" className="header__logo">
-						ninalihome
-					</Link>
-					<Navbar />
-					<Profile />
-				</div>
-				<hr />
-			</div>
-		</div>
-	);
+    return checkRoute();
 };
 
 export default Header;
--- client/src/components/pages/admin/login/Login.jsx
+++ client/src/components/pages/admin/login/Login.jsx
@@ -1,18 +1,53 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { signIn } from "../../../../actions/adminActions";
 import "./login.css";
 
 function Login() {
+    const dispatch = useDispatch();
+
+    const initialState = {
+        name: null,
+        password: null,
+    };
+
+    const [user, setUser] = useState(initialState);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(signIn(user));
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUser((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
     return (
         <div className="container">
             <div className="login--section">
-                <form className="form--login form-section">
+                <form
+                    onSubmit={handleSubmit}
+                    className="form--login form-section"
+                >
                     <label className="form--label">
                         Login
-                        <input type="text" />
+                        <input
+                            type="text"
+                            name="name"
+                            onChange={(e) => handleChange(e)}
+                        />
                     </label>
                     <label className="form--label">
                         Password
-                        <input type="text" />
+                        <input
+                            type="text"
+                            name="password"
+                            onChange={(e) => handleChange(e)}
+                        />
                     </label>
                     <input
                         type="submit"
