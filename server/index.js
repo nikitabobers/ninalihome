056cc62e785aa19efe0fa90f5d8c96c26f10f80b
nikita <nikita.bobers@gmail.com>
2020-09-29 08:44:55 +0300

    Server index.js production path

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 process.env.NODE_CONFIG_DIR = "./server/config";
-// process.env.NODE_ENV = "../client/build";
 const bodyParser = require("body-parser");
 const express = require("express");
 const path = require("path");
@@ -14,16 +13,17 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(bodyParser.json());
-// app.use(express.static("public"));
+app.use(express.static("public"));
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 
-// app.use(express.static("../client/build"));
-app.use(express.static(path.join(__dirname, "/../client/build")));
-app.get("*", (req, res) => {
-	res.sendFile(path.join(`${__dirname}/../client/build/index.html`));
-});
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("../client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname + "../client/build/index.html"));
+	});
+}
 
 app.listen(PORT, () => {
 	console.log(`Sever run at port ${PORT}`);
