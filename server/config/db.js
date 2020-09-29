8533f8119757d0861f0def5424a8c6da45127eba
nikita <nikita.bobers@gmail.com>
2020-09-29 10:49:18 +0300

    Heroky deploy 1.0

--- server/config/db.js
+++ server/config/db.js
@@ -1,19 +1,19 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = process.env.MONGODB_URI || config.get("mongoURI");
+const db = config.get("mongoURI");
 
 const connectDB = async () => {
-	try {
-		mongoose.connect(db, {
-			useNewUrlParser: true,
-			useCreateIndex: true,
-			useUnifiedTopology: true,
-			useFindAndModify: false,
-		});
-		console.log("Mongoose connected");
-	} catch (err) {
-		console.error(err.message);
-	}
+    try {
+        mongoose.connect(db, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+        });
+        console.log("Mongoose connected");
+    } catch (err) {
+        console.error(err.message);
+    }
 };
 
 module.exports = connectDB;
