0270a2300da2dcef16ad01676adf02faa37134cc
nikita <nikita.bobers@gmail.com>
2020-09-29 14:17:32 +0300

    Heroky deploy 2.0

--- server/index.js
+++ server/index.js
@@ -2,15 +2,25 @@ process.env.NODE_CONFIG_DIR = "./server/config";
 const bodyParser = require("body-parser");
 const express = require("express");
 const path = require("path");
+const config = require("config");
 const app = express();
+const mongoose = require("mongoose");
 const userRoute = require("./routes/userRoute");
 const authRoute = require("./routes/authRoute");
 const productRoute = require("./routes/productRoute");
-const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
+const db = process.env.MONGODB_URI || config.get("mongoURI");
 
-connectDB();
+mongoose
+    .connect(db, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log("MongoDB Connected..."))
+    .catch((err) => console.log(err));
 
 app.use(bodyParser.json());
 // app.use(express.static("public"));
