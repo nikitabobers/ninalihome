c30dc6521bae4eb101a00dbc525a379624b0497f
nikita <nikita.bobers@gmail.com>
2020-09-29 14:50:57 +0300

    Heroky deploy 4.0

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 
-app.use(express.static("../client/build"));
+app.use(express.static("./client/build"));
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/../client/build/index.html"));
 });
