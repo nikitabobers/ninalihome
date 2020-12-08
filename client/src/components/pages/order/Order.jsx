015e288fb30023bad4421f5fb06a9a69103f3457
nikita <nikita.bobers@gmail.com>
2020-12-08 17:00:36 +0200

    Button Back to cart added

--- client/src/components/pages/order/Order.jsx
+++ client/src/components/pages/order/Order.jsx
@@ -8,70 +8,73 @@ import { OrderShippingItem } from "./orderShippingItem/OrderShippingItem";
 import "./order.css";
 
 function Order() {
-	const cart = useSelector((state) => state.cart);
+  const cart = useSelector((state) => state.cart);
 
-	const { cartItems, loading, shipping, total } = cart;
+  const { cartItems, loading, shipping, total } = cart;
 
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(countTotalPrice());
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(countTotalPrice());
+  }, [dispatch]);
 
-	const shippingInfo = (shipping) => {
-		if (shipping) {
-			return Object.keys(shipping).map((key, keyIndex) => {
-				return (
-					<OrderShippingItem key={keyIndex} label={key} value={shipping[key]} />
-				);
-			});
-		} else {
-			return <Loader />;
-		}
-	};
-	useEffect(() => {
-		shippingInfo();
-	}, []);
+  const shippingInfo = (shipping) => {
+    if (shipping) {
+      return Object.keys(shipping).map((key, keyIndex) => {
+        return (
+          <OrderShippingItem key={keyIndex} label={key} value={shipping[key]} />
+        );
+      });
+    } else {
+      return <Loader />;
+    }
+  };
+  useEffect(() => {
+    shippingInfo();
+  }, []);
 
-	return (
-		<div className="container">
-			{loading ? (
-				<Loader />
-			) : (
-				<div className="order__section">
-					<div className="order__bag">
-						<h2 className="">Bag</h2>
-						{cartItems.map((item) => (
-							<OrderItem
-								key={item.id}
-								name={item.name}
-								price={item.price}
-								image={item.images[0]}
-								categories={item.categories}
-								color={item.color}
-								size={item.size}
-								qty={item.qty}
-							/>
-						))}
-					</div>
-					<div className="order__shipping">
-						<h2 className="">Shipping info</h2>
-						<div>{shippingInfo(shipping)}</div>
-					</div>
-					<div className="order__checkout">
-						<h2 className="">Summary</h2>
-						<div className="cart__section-total">
-							<p>Total:</p>
-							<span>{cartItems.length > 0 ? total : 0}$</span>
-						</div>
-						<Link className="btn btn--black btn--link" to="/order">
-							Place Order
-						</Link>
-					</div>
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div className="container">
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="order__section">
+          <div className="order__bag">
+            <h2 className="">Bag</h2>
+            {cartItems.map((item) => (
+              <OrderItem
+                key={item.id}
+                name={item.name}
+                price={item.price}
+                image={item.images[0]}
+                categories={item.categories}
+                color={item.color}
+                size={item.size}
+                qty={item.qty}
+              />
+            ))}
+          </div>
+          <div className="order__shipping">
+            <h2 className="">Shipping info</h2>
+            <div>{shippingInfo(shipping)}</div>
+          </div>
+          <div className="order__checkout">
+            <h2 className="">Summary</h2>
+            <div className="cart__section-total">
+              <p>Total:</p>
+              <span>{cartItems.length > 0 ? total : 0}$</span>
+            </div>
+            <Link className="btn btn--black btn--link" to="/order">
+              Place Order
+            </Link>
+            <Link className="btn btn--grey btn--link" to="/cart">
+              Back to Cart
+            </Link>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Order;
--- client/src/components/pages/product/Product.jsx
+++ client/src/components/pages/product/Product.jsx
@@ -8,127 +8,127 @@ import "./product.css";
 import DropDown from "../../layout/dropDown/DropDown";
 
 const Product = (props) => {
-	const id = props.match.params.id;
+  const id = props.match.params.id;
 
-	const dispatch = useDispatch();
-	const cart = useSelector((state) => state.cart);
-	const productDetails = useSelector((state) => state.productDetails);
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const productDetails = useSelector((state) => state.productDetails);
 
-	const { cartItems } = cart;
+  const { cartItems } = cart;
 
-	const { product, loading } = productDetails;
+  const { product, loading } = productDetails;
 
-	useEffect(() => {
-		dispatch(detailsProduct(id));
-		// eslint-disable-next-line
-	}, []);
+  useEffect(() => {
+    dispatch(detailsProduct(id));
+    // eslint-disable-next-line
+  }, []);
 
-	const cartProduct = {
-		id: product._id,
-		name: product.name,
-		categories: product.categories,
-		images: product.images,
-		price: product.price,
-		sizes: product.sizes,
-		qty: 1,
-	};
+  const cartProduct = {
+    id: product._id,
+    name: product.name,
+    categories: product.categories,
+    images: product.images,
+    price: product.price,
+    sizes: product.sizes,
+    qty: 1,
+  };
 
-	const buyItem = (product) => {
-		dispatch(addToCart(product));
-		props.history.push("/cart");
-	};
+  const buyItem = (product) => {
+    dispatch(addToCart(product));
+    props.history.push("/cart");
+  };
 
-	const goToCart = () => {
-		props.history.push("/cart");
-	};
+  const goToCart = () => {
+    props.history.push("/cart");
+  };
 
-	const goToContact = () => {
-		props.history.push("/contact");
-	};
+  const goToContact = () => {
+    props.history.push("/contact");
+  };
 
-	const buttonType = (text, action, path = null) => {
-		return (
-			<Button
-				buttonStyle="btn--order"
-				onClick={() => {
-					action(path);
-				}}
-			>
-				{text}
-			</Button>
-		);
-	};
+  const buttonType = (text, action, path = null) => {
+    return (
+      <Button
+        buttonStyle="btn--order"
+        onClick={() => {
+          action(path);
+        }}
+      >
+        {text}
+      </Button>
+    );
+  };
 
-	//Check if product is in the cart
-	let button;
-	if (product.stock) {
-		if (cartItems.length < 1) {
-			button = buttonType("Buy Item", buyItem, cartProduct);
-		}
-		for (let item of cartItems) {
-			if (item.id === cartProduct.id) {
-				button = buttonType("Checkout Cart", goToCart);
-				break;
-			} else {
-				button = buttonType("Buy Item", buyItem, cartProduct);
-			}
-		}
-	} else {
-		button = buttonType("Order Item", goToContact);
-	}
+  //Check if product is in the cart
+  let button;
+  if (product.stock) {
+    if (cartItems.length < 1) {
+      button = buttonType("Buy Item", buyItem, cartProduct);
+    }
+    for (let item of cartItems) {
+      if (item.id === cartProduct.id) {
+        button = buttonType("Checkout Cart", goToCart);
+        break;
+      } else {
+        button = buttonType("Buy Item", buyItem, cartProduct);
+      }
+    }
+  } else {
+    button = buttonType("Order Item", goToContact);
+  }
 
-	//Check if product is available
-	let status;
+  //Check if product is available
+  let status;
 
-	if (product.stock) {
-		status = <p className="success">In Stock</p>;
-	} else {
-		status = <p className="danger">Out of Stock</p>;
-	}
+  if (product.stock) {
+    status = <p className="success">In Stock</p>;
+  } else {
+    status = <p className="danger">Out of Stock</p>;
+  }
 
-	return loading ? (
-		<Loader />
-	) : (
-		<div className="container">
-			<div className="product__section">
-				<div className="product__images">
-					{product.images &&
-						product.images.map((image, index) => (
-							<img key={index} src={image} alt={product.name} />
-						))}
-				</div>
-				<div className="product__info">
-					<div className="product__info-top">
-						{status}
-						<p>{product.price}$</p>
-					</div>
-					<div className="product__name">
-						<h1>{product.name}</h1>
-					</div>
-					<div className="product__colors">
-						<p>4 colors</p>
-					</div>
-					<div className="product__description">
-						<p>{product.description}</p>
-					</div>
-					<div className="product__button">{button}</div>
-					{product.materials && (
-						<div className="product__sizeMaterial--dropdown">
-							<DropDown
-								title="Materials and Dimensions"
-								list={product.materials}
-							/>
-						</div>
-					)}
-					{product.time && (
-						<div className="product__delivery">
-							<DropDown title="Delivery" list={product.time} />
-						</div>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+  return loading ? (
+    <Loader />
+  ) : (
+    <div className="container">
+      <div className="product__section">
+        <div className="product__images">
+          {product.images &&
+            product.images.map((image, index) => (
+              <img key={index} src={image} alt={product.name} />
+            ))}
+        </div>
+        <div className="product__info">
+          <div className="product__info-top">
+            {status}
+            <p>{product.price}$</p>
+          </div>
+          <div className="product__name">
+            <h1>{product.name}</h1>
+          </div>
+          <div className="product__colors">
+            <p>4 colors</p>
+          </div>
+          <div className="product__description">
+            <p>{product.description}</p>
+          </div>
+          <div className="product__button">{button}</div>
+          {product.materials && (
+            <div className="product__sizeMaterial--dropdown">
+              <DropDown
+                title="Materials and Dimensions"
+                list={product.materials}
+              />
+            </div>
+          )}
+          {product.time && (
+            <div className="product__delivery">
+              <DropDown title="Delivery" list={product.time} />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
